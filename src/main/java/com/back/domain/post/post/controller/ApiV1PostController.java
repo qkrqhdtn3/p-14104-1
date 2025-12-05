289b17ec7f3b943d9533a2f1857112af45aa22a3
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-05 12:54:34 +0900

    11

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -5,6 +5,7 @@ import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +16,8 @@ import java.util.List;
 public class ApiV1PostController {
     private final PostService postService;
 
-    @ResponseBody
+    @GetMapping
+    @Transactional(readOnly = true)
     public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
@@ -26,6 +28,7 @@ public class ApiV1PostController {
     }
 
     @GetMapping("/{id}")
+    @Transactional(readOnly = true)
     public PostDto getItem(@PathVariable int id) {
         Post post = postService.findById(id).get();
 
