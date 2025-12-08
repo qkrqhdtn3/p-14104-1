4692a02824d6c038e3dc6c731136c3532ef714a1
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-08 12:35:26 +0900

    19

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -3,4 +3,10 @@ package com.back.global.rsData;
 import com.back.domain.post.postComment.dto.PostCommentDto;
 
 public record RsData<T>(String resultCode, String msg, T data) {
+    public RsData(String resultCode, String formatted) {
+        this(resultCode, formatted, null);
+    }git add .;
+    git commit -m "19";
+    git push origin main;
+
 }
