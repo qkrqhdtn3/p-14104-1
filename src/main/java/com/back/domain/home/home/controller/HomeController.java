ee723954fec165763a441f187c095562325e00a9
unknown <qkrqhdtn3@gmail.com>
2025-12-12 22:49:07 +0900

    65

--- src/main/java/com/back/domain/home/home/controller/HomeController.java
+++ src/main/java/com/back/domain/home/home/controller/HomeController.java
@@ -29,4 +29,28 @@ public class HomeController {
                 </div>
                 """.formatted(localHost.getHostName(), localHost.getHostAddress());
     }
+
+    @GetMapping(value = "/test/fetchPosts", produces = TEXT_HTML_VALUE)
+    @Operation(summary = "")
+    public String testFetchPosts() {
+        return """
+                <script>
+                console.clear();
+                
+                fetch("/api/v1/posts")
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                    console.log(data[1].title);
+                  });
+                
+                fetch("/api/v1/posts/1")
+                  .then(response => response.json())
+                  .then(data => {
+                    console.log(data);
+                  });
+                </script>
+                """;
+
+    }
 }
